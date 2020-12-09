68ae95d28d1bbed7e50b57ea0c278263dc52aff9
Robert Nevitt <gitcommits@robertnevitt.com>
2020-12-09 16:15:35 +0100

    First commit

---
+++ src/main/java/com/robertnevitt/plangenerator/PlangeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.robertnevitt.plangenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlangeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlangeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertnevitt/plangenerator/PlangeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertnevitt.plangenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlangeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
